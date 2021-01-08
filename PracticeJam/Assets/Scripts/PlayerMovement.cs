ba1e4eb4776cd7498799316fe9f61caec34360b0
John Sanchez <johnmarion1126@gmail.com>
2021-01-08 11:55:45 -0800

    added script to sequence animation with controller

--- PracticeJam/Assets/Scripts/PlayerMovement.cs
+++ PracticeJam/Assets/Scripts/PlayerMovement.cs
@@ -6,20 +6,46 @@ public class PlayerMovement : MonoBehaviour
 {
     
     public float moveSpeed = 5f; 
-    public Rigidbody2D rb;
+    public Rigidbody2D rigidBody;
+    public Animator animator;
+    public SpriteRenderer spriteRenderer;
+
 
     Vector2 movement;
 
     void Start() {
-        
+        animator = GetComponent<Animator>();
+        rigidBody = GetComponent<Rigidbody2D>();
     }
 
     void Update() {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+
+        if (movement.x == 1) {
+            animator.Play("HeroWalk");
+            spriteRenderer.flipX = false;
+        }
+        else if (movement.x == -1) {
+            animator.Play("HeroWalk");
+            spriteRenderer.flipX = true;
+        }
+        else if (Input.GetKey("z")) {
+            animator.Play("HeroPunch");
+            Debug.Log("Punch");
+        }
+        else if (Input.GetKey("x")) {
+            animator.Play("HeroJab");
+            Debug.Log("Jab");
+        }
+        else {
+            animator.Play("HeroIdle");
+        }
+
     }
 
     void FixedUpdate() {
-        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+        rigidBody.MovePosition(rigidBody.position + movement * moveSpeed * Time.fixedDeltaTime);
     }
+    
 }
