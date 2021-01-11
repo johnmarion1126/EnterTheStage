747d3ace992514a9d3bb5b1f08903f918d9b1de3
John Sanchez <johnmarion1126@gmail.com>
2021-01-10 16:55:51 -0800

    implemented walking animation to enemy and edited script to allow sprite to flip on its y axis to follow player

--- PracticeJam/Assets/Scripts/Enemy/EnemyMovement.cs
+++ PracticeJam/Assets/Scripts/Enemy/EnemyMovement.cs
@@ -5,13 +5,17 @@ using UnityEngine;
 public class EnemyMovement : MonoBehaviour
 {
     public Transform player;
-    private Rigidbody2D rigidbodyEnemy;
     private Vector2 movement;
+
+    private Rigidbody2D rigidbodyEnemy;
+    public Animator animatorEnemy;
+    public SpriteRenderer spriteRendererEnemy;
 
     public float moveEnemySpeed = 2f;
 
     void Start() {
         rigidbodyEnemy = GetComponent<Rigidbody2D>();
+        animatorEnemy = GetComponent<Animator>();
     }
 
     void Update() {
@@ -25,8 +29,16 @@ public class EnemyMovement : MonoBehaviour
     }
 
     void moveEnemy(Vector2 direction) {
+        
+        if (direction.x > 0) {
+            spriteRendererEnemy.flipX = false;
+        }
+        else {
+            spriteRendererEnemy.flipX = true;
+        }
+
+        animatorEnemy.Play("BasicEnemyWalk");
         rigidbodyEnemy.MovePosition((Vector2)transform.position + (Vector2)(direction * moveEnemySpeed * Time.deltaTime));
     }
-
 
 }
