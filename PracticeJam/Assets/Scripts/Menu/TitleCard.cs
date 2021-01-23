008b39270b3a78b4c6a2562f1b7b20b7413ce654
John Sanchez <johnmarion1126@gmail.com>
2021-01-23 12:14:19 -0800

    added fade in/out and scene transitions

--- PracticeJam/Assets/Scripts/Menu/TitleCard.cs
+++ PracticeJam/Assets/Scripts/Menu/TitleCard.cs
@@ -1,35 +1,57 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class TitleCard : MonoBehaviour
 {
     public GameObject title;
     public GameObject playText;
+    public Image fade;
+    public string scene;
     private bool inScene = false;
     private bool delay = false;
+
+    void Awake() {
+        fade.canvasRenderer.SetAlpha(1.0f);
+        fadeIn();
+    }
 
     void Start() {
         StartCoroutine(startSequence());
     }
 
     void Update() {
+
         if(title.activeInHierarchy && inScene == false && delay == true) {
             StartCoroutine(startPlay());
         }
+
+        if(Input.GetKey("z")) StartCoroutine(fadeOut());
+    }
+
+    void fadeIn() {
+        fade.CrossFadeAlpha(0,2,false);
+    }
+
+    IEnumerator fadeOut() {
+        fade.CrossFadeAlpha(1,2,false);
+        yield return new WaitForSeconds(2f);
+        SceneManager.LoadScene(scene);
     }
 
     IEnumerator startSequence() {
         yield return new WaitForSeconds(5f);
         title.SetActive(true);
-        yield return new WaitForSeconds(2.5f);
+        yield return new WaitForSeconds(5f);
         delay = true;
     }
 
     IEnumerator startPlay() {
         inScene = true;
         playText.SetActive(true);
-        yield return new WaitForSeconds(0.8f);
+        yield return new WaitForSeconds(0.7f);
         playText.SetActive(false);
         yield return new WaitForSeconds(0.2f);
         inScene = false;
