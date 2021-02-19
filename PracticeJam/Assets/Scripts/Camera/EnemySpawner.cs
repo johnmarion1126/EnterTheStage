4baa9d74ebb03b05512e5f610cc799017360de76
John Sanchez <johnmarion1126@gmail.com>
2021-02-18 16:03:28 -0800

    created the basic flow of the game, spawned some enemies whenever player reaches a certain distance

--- PracticeJam/Assets/Scripts/Camera/EnemySpawner.cs
+++ PracticeJam/Assets/Scripts/Camera/EnemySpawner.cs
@@ -18,12 +18,33 @@ public class EnemySpawner : MonoBehaviour
         if (playerObject.transform.position.x <= -42.0f && playerObject.transform.position.x >= -43.0f && levelPhases == 0) {
             levelPhases += 1;
             Instantiate(enemyObject1, new Vector3(-30.0f, 1.0f, 0f), Quaternion.identity);
-            Instantiate(enemyObject1, new Vector3(-30.0f, -1.5f, 0f), Quaternion.identity);
+            Instantiate(enemyObject1, new Vector3(-52.0f, -1.5f, 0f), Quaternion.identity);
+        }
+
+        if (playerObject.transform.position.x <= -16.0f && playerObject.transform.position.x >= -17.0f && levelPhases == 1) {
+            levelPhases += 1;
+            Instantiate(enemyObject1, new Vector3(-4.0f, 0.5f, 0f), Quaternion.identity);
+            Instantiate(enemyObject1, new Vector3(-26.0f, -1.0f, 0f), Quaternion.identity);
+            Instantiate(enemyObject1, new Vector3(4.0f, 1.0f, 0f), Quaternion.identity);
+        }
+
+        if (playerObject.transform.position.x <= 10.0f && playerObject.transform.position.x >= 9.0f && levelPhases == 2) {
+            levelPhases += 1;
+            Instantiate(enemyObject1, new Vector3(22.0f, -0.5f, 0f), Quaternion.identity);
+            Instantiate(enemyObject1, new Vector3(0.0f, -1.0f, 0f), Quaternion.identity);
+            Instantiate(enemyObject1, new Vector3(30.0f, 1.0f, 0f), Quaternion.identity);
+        }
+
+        if (playerObject.transform.position.x <= 40.0f && playerObject.transform.position.x >= 39.0f && levelPhases == 3) {
+            levelPhases += 1;
+            Instantiate(enemyObject1, new Vector3(52.0f, -2.0f, 0f), Quaternion.identity);
+            Instantiate(enemyObject1, new Vector3(28.0f, 1.0f, 0f), Quaternion.identity);
+            Instantiate(enemyObject1, new Vector3(23.0f, -1.0f, 0f), Quaternion.identity);
+            Instantiate(enemyObject1, new Vector3(61.0f, -2.0f, 0f), Quaternion.identity);
         }
 
         //TODO: 
-        //FIX OVERLAPPING LAYERS FOR ENEMIES
-        //REMOVE BLACK DIALOG BOX, ONLY ACTIVATE IT WHEN ENEMIES ARE TALKING
+        //ADD FIRST BOSS
         //ADD PICKUPS TO INCREASE SCORE
     }
 }
