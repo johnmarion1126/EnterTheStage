1583a7e8bbbb13b77ba3924120a7096b1c00da19
johnmarion1126 <48599206+johnmarion1126@users.noreply.github.com>
2021-04-11 21:02:51 -0700

    added a pause in the gameplay after the transition into the third level, acts like a short cutscene

--- PracticeJam/Assets/Scripts/Menu/FinalLevel.cs
+++ PracticeJam/Assets/Scripts/Menu/FinalLevel.cs
@@ -8,20 +8,33 @@ public class FinalLevel : MonoBehaviour
     private GameObject dialogBox;
     private DialogBox dialog;
 
+    [SerializeField]
+    private GameObject player;
+    private Animator playerAnimator;
+    [SerializeField]
+    private GameObject enemy;
+
     void Awake() {
         dialog = dialogBox.GetComponent<DialogBox>();
+        playerAnimator = player.GetComponent<Animator>();
     }
 
     void Start() {
+        enemy.GetComponent<EnemyMovement>().enabled = false;
+        player.GetComponent<PlayerMovement>().enabled = false;
         StartCoroutine(startScene());
+        playerAnimator.Play("HeroIdle");
     }
 
     IEnumerator startScene() {
+        yield return new WaitForSeconds(4.0f);
         dialog.addDialog("Otto: You managed to fight your way here.");
         yield return new WaitForSeconds(4.5f);
         dialog.addDialog("Otto: But you're too late.");
         yield return new WaitForSeconds(3.0f);
         dialog.addDialog("Otto: It all ends here.");
         yield return new WaitForSeconds(3.0f);
+        enemy.GetComponent<EnemyMovement>().enabled = true;
+        player.GetComponent<PlayerMovement>().enabled = true;
     }
 }
