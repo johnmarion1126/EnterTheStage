5d29b0e5bb0b1e7bc376a996cc55badaa01555a1
johnmarion1126 <48599206+johnmarion1126@users.noreply.github.com>
2021-04-11 22:50:14 -0700

    When player collides with the dog, the game now transition to the ending scene

---
+++ PracticeJam/Assets/Scripts/Menu/Dog.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Dog : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject dogObject;
+    [SerializeField]
+    private string scene;
+
+    private GameObject dialogBox;
+    private DialogBox dialog;
+    private GameObject soundManager;
+    private SoundManager sound;
+    private GameObject screenImage;
+    private BlackScreen screen;
+    private GameObject MusicManager;
+    private FadeAudioSource music;
+
+    void Awake() {
+        dialogBox = GameObject.Find("DialogBox");
+        dialog = dialogBox.GetComponent<DialogBox>();
+        soundManager = GameObject.Find("SoundManager");
+        sound = soundManager.GetComponent<SoundManager>();
+        screenImage = GameObject.Find("/HP&Score/BlackScreen");
+        screen = screenImage.GetComponent<BlackScreen>();
+        MusicManager = GameObject.Find("Music");
+        music = MusicManager.GetComponent<FadeAudioSource>();
+    }
+
+    void OnTriggerEnter2D(Collider2D collider) {
+        if (collider.name == "Player") {
+            music.fadeSourceOut(0.0001f);
+            sound.playSound("pick");
+            dogObject.GetComponent<SpriteRenderer>().enabled = false;
+            StartCoroutine(transitionToEnd());
+        }
+    }
+
+    private IEnumerator transitionToEnd() {
+        dialog.addDialog("You saved your dog!");
+        yield return new WaitForSeconds(4.0f);
+        dialog.addDialog("Time to go home :D");
+        yield return new WaitForSeconds(3.0f);
+        StartCoroutine(screen.fadeOutScreen());
+        yield return new WaitForSeconds(1.0f);
+        SceneManager.LoadScene(scene);
+        Debug.Log("Hello");
+    }
+}
