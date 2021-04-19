fab563b64b1520ec298d13d2a4daa07bdaca590d
johnmarion1126 <48599206+johnmarion1126@users.noreply.github.com>
2021-04-19 11:48:58 -0700

    initial version of game

--- Assets/Scripts/Menu/FinalLevel.cs
+++ Assets/Scripts/Menu/FinalLevel.cs
@@ -28,10 +28,10 @@ public class FinalLevel : MonoBehaviour
 
     IEnumerator startScene() {
         yield return new WaitForSeconds(2.0f);
-        dialog.addDialog("Otto: You managed to fight your way here.");
-        yield return new WaitForSeconds(4.0f);
+        dialog.addDialog("Otto: You managed to fight your way in.");
+        yield return new WaitForSeconds(5.0f);
         dialog.addDialog("Otto: But you're too late.");
-        yield return new WaitForSeconds(4.0f);
+        yield return new WaitForSeconds(3.0f);
         dialog.addDialog("Otto: It all ends here.");
         yield return new WaitForSeconds(3.0f);
         enemy.GetComponent<HoodieEnemy>().enabled = true;
